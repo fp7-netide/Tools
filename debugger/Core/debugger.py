a64bbb2249f885c6fad64770ae3e6e244b3e53d6
ElisaRojas <elisa.rojsan@hotmail.com>
2016-03-15 15:27:29 +0100

    The Logger is now human readable

--- debugger/Core/debugger.py
+++ debugger/Core/debugger.py
@@ -4,31 +4,44 @@ import sys
 import time
 import binascii
 from scapy.utils import wrpcap
-import argparse
+
+sys.path.insert(0,'../../../Engine/libraries/netip/python/')
+sys.path.insert(0,'../../../ryu/ryu/')
+
+from netip import *
+from ofproto import ofproto_parser
+from ofproto import ofproto_common
+from ofproto import ofproto_protocol
+from ofproto import ofproto_v1_0, ofproto_v1_0_parser
+from ofproto import ofproto_v1_2, ofproto_v1_2_parser
+from ofproto import ofproto_v1_3, ofproto_v1_3_parser
+from ofproto import ofproto_v1_4, ofproto_v1_4_parser
+from ofproto import ofproto_v1_5, ofproto_v1_5_parser
+
 
 ###################### headers for pcap creation ####################################
 
 #Global header for pcap 2.4
-pcap_global_header =   ('D4 C3 B2 A1'
-                        '02 00'         #File format major revision (i.e. pcap <2>.4)
-                        '04 00'         #File format minor revision (i.e. pcap 2.<4>)
-                        '00 00 00 00'
-                        '00 00 00 00'
-                        'FF FF 00 00'
+pcap_global_header =   ('D4 C3 B2 A1'   
+                        '02 00'         #File format major revision (i.e. pcap <2>.4)  
+                        '04 00'         #File format minor revision (i.e. pcap 2.<4>)   
+                        '00 00 00 00'     
+                        '00 00 00 00'     
+                        'FF FF 00 00'     
                         '93 00 00 00') #user_protocol selected, without Ip and tcp headers
 
 #pcap packet header that must preface every packet
-pcap_packet_header =   ('AA 77 9F 47'
-                        '90 A2 04 00'
-                        'XX XX XX XX'   #Frame Size (little endian)
+pcap_packet_header =   ('AA 77 9F 47'     
+                        '90 A2 04 00'     
+                        'XX XX XX XX'   #Frame Size (little endian) 
                         'YY YY YY YY')  #Frame Size (little endian)
 
 #netide packet header that must preface every packet
 netide_header =   ('01'                 #netide protocol version 1.1
                    '11'                 #openflow type
-                   'XX XX'              #Frame Size (little endian)
-                   '01 00 00 00'        #xid
-                   '00 00 00 00 00 00 00 06') #datapath_id
+                   'XX XX'              #Frame Size (little endian) 
+                   '01 00 00 00'        #xid 
+                   '00 00 00 00 00 00 00 06') #datapath_id   
 
 ######################################################################################
 
@@ -37,7 +50,7 @@ def getByteLength(str1):
     return len(''.join(str1.split())) / 2
 #    return len(str1)
 
-def generatePCAP(message,i):
+def generatePCAP(message,i): 
 
     msg_len = getByteLength(message)
 #    netide = netide_header.replace('XX XX',"%04x"%msg_len)
@@ -68,14 +81,8 @@ def sum_one(i):
 
 ##############################################################################
 
-parser = argparse.ArgumentParser(description='Launch the NetIDE debugger')
-parser.add_argument('-o', help='Output Folder', default=".")
-
-args = parser.parse_args()
-
-
-fo = open(args.o+"/results.txt", "wb")
-bitout = open(args.o+"/results.pcap", 'wb')
+fo = open("results.txt", "wb")
+bitout = open("results.pcap", 'wb')
 #msg = binascii.hexlify('hello')
 
 # Socket to talk to server
@@ -84,16 +91,50 @@ socket = context.socket(zmq.SUB)
 socket.connect("tcp://localhost:5557")
 socket.setsockopt(zmq.SUBSCRIBE, "")
 i = 0
+
 print ' [*] Waiting for logs. To exit press CTRL+C'
 while True:
     device_id, msg = socket.recv_multipart()
     device_id_str = str(device_id)
     msg_str = str(msg)
-#    print "-message " + str(msg) + "  received from " + device_id_str
+    decoded_header = NetIDEOps.netIDE_decode_header(msg)
+    message_data = msg[NetIDEOps.NetIDE_Header_Size:]
+    ret = bytearray(message_data)
+
+
+
+    datapath = decoded_header[NetIDEOps.NetIDE_header['DPID']]
+    #msg_type = decoded_header[NetIDEOps.NetIDE_header['DPID']]
+    #msg_len = decoded_header[NetIDEOps.NetIDE_header['DPID']]
+    #xid = decoded_header[NetIDEOps.NetIDE_header['DPID']]
+    
+
+    if len(ret) >= ofproto_common.OFP_HEADER_SIZE:
+       (version, msg_type, msg_len, xid) = ofproto_parser.header(ret)
+       msg_decoded = ofproto_parser.msg(datapath, version, msg_type, msg_len, xid, ret)
+
+    #msg_decimal = []
+    #msg_ascii = []
+    #for a in msg:
+    #   msg_decimal.append(str(ord(a)))
+
+    #for a in msg_decimal:
+    #   msg_ascii.append(str(a))
+       #print str(a)+"\n"
+    # print "-message " + str(msg) + "  received from " + device_id_str
     t=time.strftime("%H:%M:%S")
+    #msg_decimal = convert_to_decimal(msg)
     if device_id_str[2:] == "shim":
-        print "msg from shim"
-        print '\033[1;32m[%r] [%r] %r\033[1;m'% (t, device_id_str, msg)
+        if 'msg_decoded' in locals() or 'msg_decoded' in globals():
+           print "msg from shim"
+           #print version
+           print '\033[1;32m[%r] [%r] %r\033[1;m'% (t, device_id_str, msg_decoded)+'\n'
+           #print ' '.join([str(ord(a)) for a in msg_str])
+           #print ' '.join(unicode(a, "utf-8") for a in msg_str)
+           #print ' '.join(a.decode('ascii') for a in msg)
+           #print binascii.hexlify(msg).decode("ascii")
+           #print ' '.join(a.decode('hex') for a in msg)
+           #print type(msg_str)
         fo.write("[%r] [%r] %r \n"% (t, device_id_str, msg));
         msg_cap = binascii.hexlify(msg)
         #global i
@@ -102,13 +143,14 @@ while True:
         i = sum_one(i)
         #print bytestring
         bytelist = bytestring.split()
-        #print bytelist
+        #print bytelist  
         bytes = binascii.a2b_hex(''.join(bytelist))
         #print bytes
         bitout.write(bytes);
     else:
-        print "msg from backend"
-        print '\033[1;33m[%r] [%r] %r\033[1;m'% (t, device_id_str, msg)
+        if 'msg_decoded' in locals() or 'msg_decoded' in globals():
+           print "msg from backend"
+           print '\033[1;33m[%r] [%r] %r\033[1;m'% (t, device_id_str, msg_decoded)+'\n'
         fo.write("[%r] [%r] %r\n"% (t, device_id_str, msg));
         msg_cap = binascii.hexlify(msg)
         #print i
@@ -116,7 +158,7 @@ while True:
         i = sum_one(i)
         #print bytestring
         bytelist = bytestring.split()
-        #print bytelist
+        #print bytelist  
         bytes = binascii.a2b_hex(''.join(bytelist))
         #print bytes
         bitout.write(bytes);
