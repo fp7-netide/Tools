43d830505d9c2838e81aa7f49403b5454a5f3178
sergiotamu <sergio.tamu@gmail.com>
2016-11-05 18:55:51 +0100

    File that simulates the logic that should be implemented on IDE for receiving the info of the Network Profiler

--- profiler/Network_Profiler/test.py
+++ profiler/Network_Profiler/test.py
@@ -1,6 +1,7 @@
 import sys
 import zmq
 import time
+import json
 
 sys.path.insert(0,'../../../Engine/libraries/netip/python/')
 
@@ -9,7 +10,7 @@ from netip import *
 # Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:5560")
+socket.connect("tcp://localhost:5561")
 socket.setsockopt(zmq.SUBSCRIBE, "")
 #time.sleep(3)
 
@@ -18,5 +19,14 @@ while True:
    netip_msg = socket.recv()
    #print(netip_msg)
    (netide_version, netide_msg_type, netide_msg_len, netide_xid, netide_mod_id, netide_datapath) = NetIDEOps.netIDE_decode_header(netip_msg)
-   verificator_msg = netip_msg[NetIDEOps.NetIDE_Header_Size:]
-   print(verificator_msg)
+   stat_msg = netip_msg[NetIDEOps.NetIDE_Header_Size:]
+
+   #decoded_stats = json.loads(verificator_msg)
+   #for stat in stat_msg:
+   decoded_stats = json.loads(stat_msg)
+   print("DECODED AGGREGATE FLOWS:", decoded_stats)
+   #for i in decoded_stats:
+      #stat = json.loads(i)
+      #print("DECODED:", stat[2]["port"])
+      #print("DECODED FLOW STATS:", stat)
+   #print(verificator_msg)
